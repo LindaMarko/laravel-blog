b6dbdf816beeb4ad8a9d7fc87fc2dbb3ae2c3853
Linda Marko <lindamarko83@gmail.com>
2022-07-16 09:08:58 +0200

    Add Home and About views

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Blog</title>
+  </head>
+  <body>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/about">About</a></li>
+    </ul>
+    <div class="main">
+      <h1>Home page</h1>
+      <p>This is the home page</p>
+    </div>
+  </body>
+</html>
