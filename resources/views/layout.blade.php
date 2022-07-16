37935817bf3ded518b612fd1e20fc0c56f546173
Linda Marko <lindamarko83@gmail.com>
2022-07-16 09:43:27 +0200

    Add main.css and implement active links

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,11 +4,12 @@
     <title>
       @yield('title', 'Blog')
     </title>
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
   </head>
   <body>
-    <ul>
-      <li><a href="{{ route('home') }}">Home</a></li>
-      <li><a href="{{ route('about') }}">About</a></li>
+    <ul class="nav">
+      <li><a class="{{ request()->routeIs('home') ? 'active' : ''}}" href="{{ route('home') }}">Home</a></li>
+      <li><a class="{{ request()->routeIs('about') ? 'active' : ''}}" href="{{ route('about') }}">About</a></li>
     </ul>
     <div class="main">
       @yield('content')
