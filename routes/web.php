5a6b3adc9dddd6bd722cf77f29d29daad82293d6
Linda Marko <lindamarko83@gmail.com>
2022-07-18 09:46:20 +0200

    Implement resource routes and refactor post routes

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,15 @@
 <?php
 
 use App\Http\Controllers\HomeController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'home'])->name('home');
 
 Route::get('/about', [HomeController::class, 'about'])->name('about');
 
-Route::name('posts.')->prefix('posts')->group(function () {
-    Route::get('/create', function () {
-        return view('posts.create');
-    })->name('create');
 
-    Route::post('/', function (Request $request) {
-        $request->validate([
-            'title' => 'required',
-            'description' => ['required', 'min:10'],
-        ]);
+Route::resource('posts', PostController::class)->except([
+    'index',
+]);
 
-        return redirect()
-            ->route('posts.create')
-            ->with('success', 'Post is submitted! Title: ' . $request->input('title') . ' Description: ' . $request->input('description'));
-    })->name('store');
-});
