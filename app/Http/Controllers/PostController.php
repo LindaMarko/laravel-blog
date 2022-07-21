0a8a33bf44896ea0c2f652c91337c5893c0d8501
Linda Marko <lindamarko83@gmail.com>
2022-07-21 09:14:46 +0200

    Add route model binding

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -39,8 +39,10 @@ class PostController extends Controller
         $post->save();
 
         return redirect()
-            ->route('posts.create')
-            ->with('success', 'Post is submitted! Title: ' . $post->title . ' Description: ' . $post->description);
+            ->route('posts.show', [$post])
+            ->with('success', 'Post is submitted! Title: ' .
+            $post->title . ' Description: ' .
+            $post->description);
     }
 
     /**
@@ -49,10 +51,10 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Post $post)
     {
         return view('posts.show', [
-            'post' => Post::findOrFail($id),
+            'post' => $post,
         ]);
     }
 
@@ -62,10 +64,10 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Post $post)
     {
         return view('posts.edit', [
-            'post' => Post::findOrFail($id),
+            'post' => $post,
         ]);
     }
 
@@ -76,14 +78,12 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Post $post)
     {
         $request->validate([
             'title' => 'required',
             'description' => ['required', 'min:10'],
         ]);
-
-        $post = Post::findOrFail($id);
 
         $post->title = $request->input('title');
         $post->description = $request->input('description');
@@ -91,7 +91,7 @@ class PostController extends Controller
         $post->save();
 
         return redirect()
-            ->route('posts.show', ['post' => $post->id])
+            ->route('posts.show', [$post])
             ->with('success', 'Post is updated!');
     }
 
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,6 +7,7 @@
   <div class="post-content">
     <h1>{{ $post->title }}</h1>
     <p>{{ $post->description }}</p>
+    <a href="{{ route('posts.edit', [$post]) }}">Edit post</a>
   </div>
 </div>
 @endsection
